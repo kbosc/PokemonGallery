d0098b9d263fa5a71a6d5a61222d35b8cc1e4020
kbosc <kevin_bosc@hotmail.fr>
2026-04-12 16:57:59 +0200

    [Chore] React 19 compat pass and final cleanup
    - Add TODO comment on Button forwardRef: React 19 supports ref
      as a prop, to be migrated later when pattern is more established
    - Delete dead localStoragePokemon.tsx (replaced by useCapturedPokemon
      hook in Phase 2)

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -3,6 +3,9 @@ import styles from "./button.module.css";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
+// TODO: React 19 permet de passer ref directement comme prop
+// (sans forwardRef). À migrer quand le pattern sera plus établi.
+// Voir : https://react.dev/blog/2024/12/05/react-19#ref-as-a-prop
 const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   return <button ref={ref} className={styles.button} {...props} />;
 });
